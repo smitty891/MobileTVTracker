6bad98d9d19ecfe31a02877a8f86e3b6aee40d4d
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-03-31 16:58:05 -0400

    Update IntegrationUnitTests.kt
    added in the imports

--- app/src/test/java/com/tvtracker/IntegrationUnitTests.kt
+++ app/src/test/java/com/tvtracker/IntegrationUnitTests.kt
@@ -1,7 +1,25 @@
 package com.tvtracker
 
-
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.tvtracker.dto.ImdbResponse
+import com.tvtracker.service.MediaService
+import com.tvtracker.ui.main.BrowseViewModel
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import junit.framework.Assert.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.*
+import org.junit.rules.TestRule
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 
 class IntegrationUnitTests {
+    
 }
