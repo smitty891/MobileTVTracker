f61e9fd0d5796c8e8fed93a2749be1ad0e07292f
tate <tatettw@live.com>
2022-04-08 11:04:07 -0400

    Uncomment share button

--- app/src/main/java/com/tvtracker/MainActivity.kt
+++ app/src/main/java/com/tvtracker/MainActivity.kt
@@ -424,22 +424,22 @@ class MainActivity : ComponentActivity() {
                         }
                     }, content = { Text("Favorites") }, colors = favBtnColor)
                 }
-//                Column(modifier = Modifier.padding(16.dp)) {
-//                    Button(onClick = {
-//                        val share = Intent.createChooser(Intent().apply {
-//                            action = Intent.ACTION_SEND
-//                            putExtra(Intent.EXTRA_TEXT, "Check this out")
-//
-//                            // (Optional) Here we're setting the title of the content
-//                            putExtra(Intent.EXTRA_TITLE, "See a preview")
-//
-//                            // (Optional) Here we're passing a content URI to an image to be displayed
-//                            // data = contentUri
-//                            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-//                        }, null)
-//                        startActivity(share)
-//                    }, content = { Text( "Share") })
-//                }
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Button(onClick = {
+                        val share = Intent.createChooser(Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, "Check this out")
+
+                            // (Optional) Here we're setting the title of the content
+                            putExtra(Intent.EXTRA_TITLE, "See a preview")
+
+                            // (Optional) Here we're passing a content URI to an image to be displayed
+                            // data = contentUri
+                            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        }, null)
+                        startActivity(share)
+                    }, content = { Text( "Share") })
+                }
             }
         }
     }
